7c94bc1b1a22d317f2b90ef21af75b3cdbea43ce
Mohammed-Ashwaq <85397443+Mohammed-Ashwaq@users.noreply.github.com>
2025-03-19 21:42:07 +0530

    Update button.js
    Fixed routing for signup page for desktop view

--- src/components/button.js
+++ src/components/button.js
@@ -13,7 +13,7 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
-        <Link to='/signup' className='btn-mobile'>
+        <Link to='/auth' className='btn-mobile'>
 
             <button
             className={'btn ${checkButtonStyle} ${checkButtonSize}'}
